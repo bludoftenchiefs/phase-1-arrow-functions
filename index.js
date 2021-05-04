9f44de6a22269660a0fca496edd6999bd52f67aa
rilith <bludoftenchiefs@protonmail.com>
2021-05-04 08:41:48 -0500

    commit tree c9550bdeb6ec874cecd815a26b8e074bf7b56f99

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide() {
+        let num1 = 2000;
+        let num2 = 100;
+        return num1 / num2;
+}
+
+const square = (x => x ** 2);
+
+const add = (a, b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+}
